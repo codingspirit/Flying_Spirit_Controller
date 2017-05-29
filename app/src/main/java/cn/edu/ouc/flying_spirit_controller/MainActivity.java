bad3add91d7ad3fb3de31cfb4d8eaf98db6cbea5
Alex.D.Scofield <lizhiqin46783937@live.com>
2017-05-29 21:49:14 +0800

    Ver 0.2

--- app/src/main/java/cn/edu/ouc/flying_spirit_controller/MainActivity.java
+++ app/src/main/java/cn/edu/ouc/flying_spirit_controller/MainActivity.java
@@ -1,14 +1,27 @@
 package cn.edu.ouc.flying_spirit_controller;
 
-import android.bluetooth.BluetoothManager;
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.clj.flying_spirit_controller.BleManager;
+import com.clj.fastble.BleManager;
+import com.clj.fastble.conn.BleCharacterCallback;
+import com.clj.fastble.conn.BleGattCallback;
+import com.clj.fastble.data.ScanResult;
+import com.clj.fastble.exception.BleException;
+import com.clj.flying_spirit_controller.utils.HexUtil;
 
 import java.util.Locale;
 
@@ -19,22 +32,40 @@ public class MainActivity extends AppCompatActivity {
     private SeekBar mSbAccelerator;
     private TextView mTvAccelerator;
     private Switch mSBle;
-    private BleManager bleManager;
+    private ImageView mIvLoading;
+    private Animation loadingAnim;
+    private ProgressDialog connectingDialog;
+
     private RockerState rockerDirState = new RockerState();
     private RockerState rockerAccState = new RockerState();
     private controlRunnable cRunnable = new controlRunnable();
+    private receiveRunnable rRunnable = new receiveRunnable();
     private Thread controlThread = new Thread(cRunnable);
+    private Thread receiveThread = new Thread(rRunnable);
+
+    private String connectName = "flying_spirit";
+    private String UUID_SERVICE = "00000000-0000-0000-8000-00805f9b0000";
+    private String UUID_READ = "00000000-0000-0000-8000-00805f9b0000";
+    private BleManager myBleManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //region findViewById
         mRockerViewDir = (RockerView) findViewById(R.id.rocker_dir);
         mRockerViewAccelerator = (RockerView) findViewById(R.id.rocker_accelerator);
         mSbAccelerator = (SeekBar) findViewById(R.id.sb_accelerator);
         mTvAccelerator = (TextView) findViewById(R.id.tv_accelerator);
-        mSBle=(Switch)findViewById(R.id.s_bluetooth);
+        mSBle = (Switch) findViewById(R.id.s_bluetooth);
+        mIvLoading = (ImageView) findViewById(R.id.iv_loading);
+        //endregion
+
+        myBleManager = new BleManager(this);
+
+        loadingAnim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        loadingAnim.setInterpolator(new LinearInterpolator());
 
         mRockerViewDir.setOnShakeListener(RockerView.DirectionMode.DIRECTION_8, new RockerView.OnShakeListener() {
             @Override
@@ -118,14 +149,53 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        bleManager=new BleManager(this);
         mSBle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b){
-                    bleManager.enableBluetooth();
-                }else {
-                    bleManager.disableBluetooth();
+                if (b) {
+                    if (myBleManager.isSupportBle()) {
+                        if (!myBleManager.isBlueEnable())
+                            myBleManager.enableBluetooth();
+                        mIvLoading.startAnimation(loadingAnim);
+                        myBleManager.scanNameAndConnect(connectName, 5000, true, new BleGattCallback() {
+                            @Override
+                            public void onNotFoundDevice() {
+                                mIvLoading.clearAnimation();
+                                Toast.makeText(MainActivity.this, "No Device Found", Toast.LENGTH_SHORT).show();
+                                mSBle.setChecked(false);
+                            }
+
+                            @Override
+                            public void onFoundDevice(ScanResult scanResult) {
+                                mIvLoading.clearAnimation();
+                                connectingDialog = ProgressDialog.show(MainActivity.this, "Connecting", "Connecting to" + connectName);
+                            }
+
+                            @Override
+                            public void onConnectSuccess(BluetoothGatt gatt, int status) {
+                                connectingDialog.dismiss();
+                                Toast.makeText(MainActivity.this, "Connect Success", Toast.LENGTH_SHORT).show();
+                                if(!receiveThread.isAlive()){
+                                    receiveThread.start();
+                                }
+                            }
+
+                            @Override
+                            public void onConnectFailure(BleException exception) {
+                                connectingDialog.dismiss();
+                                Toast.makeText(MainActivity.this, "Connect Failed", Toast.LENGTH_SHORT).show();
+                                mSBle.setChecked(false);
+                            }
+                        });
+                    } else {
+                        Toast.makeText(MainActivity.this, "Device Not Supported", Toast.LENGTH_SHORT).show();
+                        mSBle.setChecked(false);
+                    }
+                } else {
+                    if (myBleManager.isBlueEnable()) {
+                        myBleManager.closeBluetoothGatt();
+                        myBleManager.disableBluetooth();
+                    }
                 }
             }
         });
@@ -137,20 +207,42 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             while (rockerDirState.direction != RockerView.Direction.DIRECTION_CENTER ||
                     rockerAccState.direction != RockerView.Direction.DIRECTION_CENTER) {
-                switch (rockerAccState.direction) {
-                    case DIRECTION_UP:
-                        mSbAccelerator.setProgress(mSbAccelerator.getProgress() + rockerAccState.distance);
-                        break;
-                    case DIRECTION_DOWN:
-                        mSbAccelerator.setProgress(mSbAccelerator.getProgress() - rockerAccState.distance);
-                        break;
-                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        switch (rockerAccState.direction) {
+                            case DIRECTION_UP:
+                                mSbAccelerator.setProgress(mSbAccelerator.getProgress() + rockerAccState.distance);
+                                break;
+                            case DIRECTION_DOWN:
+                                mSbAccelerator.setProgress(mSbAccelerator.getProgress() - rockerAccState.distance);
+                                break;
+                        }
+                    }
+                });
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    private class receiveRunnable implements Runnable{
+        @Override
+        public void run() {
+            myBleManager.readDevice(UUID_SERVICE, UUID_READ, new BleCharacterCallback() {
+                @Override
+                public void onSuccess(BluetoothGattCharacteristic characteristic) {
+                    Log.d("receiveData",String.valueOf(HexUtil.encodeHex(characteristic.getValue())));
+                }
+
+                @Override
+                public void onFailure(BleException exception) {
+
+                }
+            });
         }
     }
 }
@@ -166,4 +258,5 @@ class RockerState {
         distance = 0;
     }
 }
+
 
